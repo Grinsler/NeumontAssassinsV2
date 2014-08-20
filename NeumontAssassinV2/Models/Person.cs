81a4be38eac2f7284f706a5217c7b207d0de8c62
Grinsler <reverentrunner@gmail.com>
2014-08-20 14:21:34 -0600

    added classes

---
+++ NeumontAssassinV2/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeumontAssassinV2.Models
+{
+    public class Person
+    {
+        public string Player_Name { get; set; }
+        public int MissionLevel { get; set; }
+        public int Player_Strength { get; set; }
+        public int Player_Intellegence { get; set; }
+        public int Player_Agility { get; set; }
+        public int Player_Charisma { get; set; }
+    }
+}
